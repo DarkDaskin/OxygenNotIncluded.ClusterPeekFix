7effe38c27706b4e2b7e96ec906aac83a84ff752
Dark Daskin <darkdaskin@gmail.com>
2021-03-07 00:53:06 +0300

    Add project files.

---
+++ OxygenNotIncluded.ClusterPeekFix/Patch_ClusterFogOfWarManager_Instance_Initialize.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Harmony;
+
+namespace OxygenNotIncluded.ClusterPeekFix
+{
+    [HarmonyPatch(typeof(ClusterFogOfWarManager.Instance), nameof(ClusterFogOfWarManager.Instance.Initialize))]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class Patch_ClusterFogOfWarManager_Instance_Initialize
+    {
+        public static void Postfix(ClusterFogOfWarManager.Instance __instance)
+        {
+            foreach (var location in ClusterGrid.Instance.cellContents.Keys)
+                if (__instance.IsLocationRevealed(location))
+                    __instance.PeekLocation(location, 2);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+This mod for **Oxygen Not Included: Spaced Out** fixes the starmap cells not correctly revealed when loading a save made in older builds.
+
+Without mod:
+![screenshot before](images/before.jpg)
+
+With mod:
+![screenshot after](images/after.jpg)
